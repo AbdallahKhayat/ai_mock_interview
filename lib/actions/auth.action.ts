4e4be4cc363701be455d8f92faa104f55c557ee3
AbdallahKhayat <abdullahabdkhayat@gmail.com>
2025-04-19 22:20:30 +0300

    implement some changes

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,37 +1,59 @@
-'use server'; //to turn it into server render file
+'use server';
 
-import { db, auth } from '@/firebase/admin';
+import { auth, db } from '@/firebase/admin';
 import { cookies } from 'next/headers';
 
-const ONE_WEEK = 60 * 60 * 24 * 7; // 1 week: 60 seconds 60 minutes 24 hours 7 days
+// Session duration (1 week)
+const SESSION_DURATION = 60 * 60 * 24 * 7;
+
+// Set session cookie
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+
+  // Create session cookie
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: SESSION_DURATION * 1000, // milliseconds
+  });
+
+  // Set cookie in the browser
+  cookieStore.set('session', sessionCookie, {
+    maxAge: SESSION_DURATION,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    path: '/',
+    sameSite: 'lax',
+  });
+}
 
 export async function signUp(params: SignUpParams) {
-
   const { uid, name, email } = params;
 
   try {
-
-    const userRecord = await db.collection('users').doc(uid).get(); //check if user already exist
-
-    if (userRecord.exists) {
+    // check if user exists in db
+    const userRecord = await db.collection('users').doc(uid).get();
+    if (userRecord.exists)
       return {
-        status: 'success',
-        message: 'User already exists. Please sign in instead.',
+        success: false,
+        message: 'User already exists. Please sign in.',
       };
-    }
 
+    // save user to db
     await db.collection('users').doc(uid).set({
-      name, email,
+      name,
+      email,
+      // profileURL,
+      // resumeURL,
     });
 
     return {
       success: true,
       message: 'Account created successfully. Please sign in.',
     };
-  } catch (err: any) {
-    console.log('Error Creating a user', err);
+  } catch (error: any) {
+    console.error('Error creating user:', error);
 
-    if (err.code === 'auth/email-already-exists') {
+    // Handle Firebase specific errors
+    if (error.code === 'auth/email-already-exists') {
       return {
         success: false,
         message: 'This email is already in use',
@@ -40,96 +62,208 @@ export async function signUp(params: SignUpParams) {
 
     return {
       success: false,
-      message: 'Failed to create an account',
+      message: 'Failed to create account. Please try again.',
     };
-
   }
-
 }
 
 export async function signIn(params: SignInParams) {
   const { email, idToken } = params;
 
-
   try {
-
     const userRecord = await auth.getUserByEmail(email);
-
-    if (!userRecord) {
+    if (!userRecord)
       return {
-
         success: false,
-        message: 'User does not exist. Create an account instead.',
+        message: 'User does not exist. Create an account.',
       };
-    }
 
     await setSessionCookie(idToken);
-
-  } catch (err) {
-    console.log(err);
+  } catch (error: any) {
+    console.log('');
 
     return {
       success: false,
-      message: 'Failed to log into an account',
+      message: 'Failed to log into account. Please try again.',
     };
-
   }
-
 }
 
-export async function setSessionCookie(idToken: string) {
-
+// Sign out user by clearing the session cookie
+export async function signOut() {
   const cookieStore = await cookies();
 
-  const sessionCookie = await auth.createSessionCookie(idToken, {
-    expiresIn: ONE_WEEK * 1000, // * 1000 ms
-
-  });
-  cookieStore.set('session', sessionCookie, {
-    maxAge: ONE_WEEK,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    path: '/',
-    sameSite: 'lax',
-  });
+  cookieStore.delete('session');
 }
 
-// Now to make only authenticated user access the homepage
+// Get current user from session cookie
 export async function getCurrentUser(): Promise<User | null> {
-
   const cookieStore = await cookies();
-  const sessionCookie = cookieStore.get('session')?.value;
 
-  if (!sessionCookie) {
-    return null;
-  }
+  const sessionCookie = cookieStore.get('session')?.value;
+  if (!sessionCookie) return null;
 
   try {
-// decode the session to see if we have a valid user
     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
 
-    const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
-// if no session cookie exists which means no authenticated user exists
+    // get user info from db
+    const userRecord = await db
+      .collection('users')
+      .doc(decodedClaims.uid)
+      .get();
     if (!userRecord.exists) return null;
 
-    //if exists
     return {
-      ...userRecord.data,
+      ...userRecord.data(),
       id: userRecord.id,
     } as User;
+  } catch (error) {
+    console.log(error);
 
-  } catch (err) {
-    console.log(err);
+    // Invalid or expired session
     return null;
-
   }
-
 }
 
+// Check if user is authenticated
 export async function isAuthenticated() {
   const user = await getCurrentUser();
-  return !!user; // {name:"Abdallah} -> !{}=>false=>!false=>true (boolean)
-  //or if null
-  // " " -> !" " -> true -> !true -> false
+  return !!user;
 }
 
+
+// 'use server'; //to turn it into server render file
+//
+// import { db, auth } from '@/firebase/admin';
+// import { cookies } from 'next/headers';
+//
+// const ONE_WEEK = 60 * 60 * 24 * 7; // 1 week: 60 seconds 60 minutes 24 hours 7 days
+//
+// export async function signUp(params: SignUpParams) {
+//
+//   const { uid, name, email } = params;
+//
+//   try {
+//
+//     const userRecord = await db.collection('users').doc(uid).get(); //check if user already exist
+//
+//     if (userRecord.exists) {
+//       return {
+//         status: 'success',
+//         message: 'User already exists. Please sign in instead.',
+//       };
+//     }
+//
+//     await db.collection('users').doc(uid).set({
+//       name, email,
+//     });
+//
+//     return {
+//       success: true,
+//       message: 'Account created successfully. Please sign in.',
+//     };
+//   } catch (err: any) {
+//     console.log('Error Creating a user', err);
+//
+//     if (err.code === 'auth/email-already-exists') {
+//       return {
+//         success: false,
+//         message: 'This email is already in use',
+//       };
+//     }
+//
+//     return {
+//       success: false,
+//       message: 'Failed to create an account',
+//     };
+//
+//   }
+//
+// }
+//
+// export async function signIn(params: SignInParams) {
+//   const { email, idToken } = params;
+//
+//
+//   try {
+//
+//     const userRecord = await auth.getUserByEmail(email);
+//
+//     if (!userRecord) {
+//       return {
+//
+//         success: false,
+//         message: 'User does not exist. Create an account instead.',
+//       };
+//     }
+//
+//     await setSessionCookie(idToken);
+//
+//   } catch (err) {
+//     console.log(err);
+//
+//     return {
+//       success: false,
+//       message: 'Failed to log into an account',
+//     };
+//
+//   }
+//
+// }
+//
+// export async function setSessionCookie(idToken: string) {
+//
+//   const cookieStore = await cookies();
+//
+//   const sessionCookie = await auth.createSessionCookie(idToken, {
+//     expiresIn: ONE_WEEK * 1000, // * 1000 ms
+//
+//   });
+//   cookieStore.set('session', sessionCookie, {
+//     maxAge: ONE_WEEK,
+//     httpOnly: true,
+//     secure: process.env.NODE_ENV === 'production',
+//     path: '/',
+//     sameSite: 'lax',
+//   });
+// }
+//
+// // Now to make only authenticated user access the homepage
+// export async function getCurrentUser(): Promise<User | null> {
+//
+//   const cookieStore = await cookies();
+//   const sessionCookie = cookieStore.get('session')?.value;
+//
+//   if (!sessionCookie) {
+//     return null;
+//   }
+//
+//   try {
+// // decode the session to see if we have a valid user
+//     const decodedClaims = await auth.verifySessionCookie(sessionCookie, true);
+//
+//     const userRecord = await db.collection('users').doc(decodedClaims.uid).get();
+// // if no session cookie exists which means no authenticated user exists
+//     if (!userRecord.exists) return null;
+//
+//     //if exists
+//     return {
+//       ...userRecord.data,
+//       id: userRecord.id,
+//     } as User;
+//
+//   } catch (err) {
+//     console.log(err);
+//     return null;
+//
+//   }
+//
+// }
+//
+// export async function isAuthenticated() {
+//   const user = await getCurrentUser();
+//   return !!user; // {name:"Abdallah} -> !{}=>false=>!false=>true (boolean)
+//   //or if null
+//   // " " -> !" " -> true -> !true -> false
+// }
+//
--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -1,67 +1,26 @@
 'use server';
 
-import { db } from '@/firebase/admin';
 import { generateObject } from 'ai';
 import { google } from '@ai-sdk/google';
+
+import { db } from '@/firebase/admin';
 import { feedbackSchema } from '@/constants';
 
-// function to fetch all the interviews from firebase
-export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
-  const interviews = await db.collection('interviews').where('userId', '==', userId).orderBy('createdAt', 'desc').get();
-
-  return interviews.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Interview[];
-}
-
-// function to fetch all interviews created by other users
-export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
-  const { userId, limit = 20 } = params;
-
-  const interviews = await db.collection('interviews').where('finalized', '==', true).where('userId', '!=', userId).limit(limit).get();
-
-  return interviews.docs.map((doc) => ({
-    id: doc.id,
-    ...doc.data(),
-  })) as Interview[];
-}
-
-
-// function to fetch interview details from firebase
-export async function getInterviewById(id: string): Promise<Interview | null> {
-  const interview = await db.collection('interviews').doc(id).get();
-
-  return interview.data() as Interview | null;
-}
-
-
-// Create a server action that will create feedback in firestore database using Gemini AI
 export async function createFeedback(params: CreateFeedbackParams) {
-
-  const { interviewId, userId, transcript } = params;
+  const { interviewId, userId, transcript, feedbackId } = params;
 
   try {
-// content is what the person has answered
     const formattedTranscript = transcript
       .map(
         (sentence: { role: string; content: string }) =>
           `- ${sentence.role}: ${sentence.content}\n`,
       )
       .join('');
-    const {
-      object: {
-        totalScore,
-        categoryScores,
-        strengths,
-        areasForImprovement,
-        finalAssessment,
-      },
-    } = await generateObject({
+
+    const { object } = await generateObject({
       model: google('gemini-2.0-flash-001', {
         structuredOutputs: false,
       }),
-      // schema to specify how the text will look like
       schema: feedbackSchema,
       prompt: `
         You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
@@ -75,46 +34,214 @@ export async function createFeedback(params: CreateFeedbackParams) {
         - **Cultural & Role Fit**: Alignment with company values and job role.
         - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
         `,
-      // system to describe what is this AI
       system:
         'You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories',
     });
 
-    const feedback = await db.collection('feedback').add({
-      interviewId,
-      userId,
-      totalScore,
-      categoryScores,
-      strengths,
-      areasForImprovement,
-      finalAssessment,
+    const feedback = {
+      interviewId: interviewId,
+      userId: userId,
+      totalScore: object.totalScore,
+      categoryScores: object.categoryScores,
+      strengths: object.strengths,
+      areasForImprovement: object.areasForImprovement,
+      finalAssessment: object.finalAssessment,
       createdAt: new Date().toISOString(),
-
-    });
-
-    return {
-      success: true,
-      feedbackId: feedback.id,
     };
-  } catch (err) {
-    console.error('Error saving feedback', err);
+
+    let feedbackRef;
+
+    if (feedbackId) {
+      feedbackRef = db.collection('feedback').doc(feedbackId);
+    } else {
+      feedbackRef = db.collection('feedback').doc();
+    }
+
+    await feedbackRef.set(feedback);
+
+    return { success: true, feedbackId: feedbackRef.id };
+  } catch (error) {
+    console.error('Error saving feedback:', error);
     return { success: false };
   }
-
 }
 
-// function to fetch Feedback based on interview ID
-export async function getFeedbackByInterviewId(params: GetFeedbackByInterviewIdParams): Promise<Feedback | null> {
+export async function getInterviewById(id: string): Promise<Interview | null> {
+  const interview = await db.collection('interviews').doc(id).get();
+
+  return interview.data() as Interview | null;
+}
+
+export async function getFeedbackByInterviewId(
+  params: GetFeedbackByInterviewIdParams,
+): Promise<Feedback | null> {
   const { interviewId, userId } = params;
 
-  const feedback = await db.collection('feedback').where('interviewId', '==', interviewId).where('userId', '==', userId).limit(1).get();
+  const querySnapshot = await db
+    .collection('feedback')
+    .where('interviewId', '==', interviewId)
+    .where('userId', '==', userId)
+    .limit(1)
+    .get();
 
-  if (feedback.empty) {
-    return null;
-  }
+  if (querySnapshot.empty) return null;
 
-  const feedbackDoc = feedback.docs[0];
-  return {
-    id: feedbackDoc.id, ...feedbackDoc.data(),
-  } as Feedback;
+  const feedbackDoc = querySnapshot.docs[0];
+  return { id: feedbackDoc.id, ...feedbackDoc.data() } as Feedback;
 }
+
+export async function getLatestInterviews(
+  params: GetLatestInterviewsParams,
+): Promise<Interview[] | null> {
+  const { userId, limit = 20 } = params;
+
+  const interviews = await db
+    .collection('interviews')
+    .orderBy('createdAt', 'desc')
+    .where('finalized', '==', true)
+    .where('userId', '!=', userId)
+    .limit(limit)
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+
+export async function getInterviewsByUserId(
+  userId: string,
+): Promise<Interview[] | null> {
+  const interviews = await db
+    .collection('interviews')
+    .where('userId', '==', userId)
+    .orderBy('createdAt', 'desc')
+    .get();
+
+  return interviews.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  })) as Interview[];
+}
+
+
+// 'use server';
+//
+// import { db } from '@/firebase/admin';
+// import { generateObject } from 'ai';
+// import { google } from '@ai-sdk/google';
+// import { feedbackSchema } from '@/constants';
+//
+// // function to fetch all the interviews from firebase
+// export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+//   const interviews = await db.collection('interviews').where('userId', '==', userId).orderBy('createdAt', 'desc').get();
+//
+//   return interviews.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data(),
+//   })) as Interview[];
+// }
+//
+// // function to fetch all interviews created by other users
+// export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+//   const { userId, limit = 20 } = params;
+//
+//   const interviews = await db.collection('interviews').where('finalized', '==', true).where('userId', '!=', userId).limit(limit).get();
+//
+//   return interviews.docs.map((doc) => ({
+//     id: doc.id,
+//     ...doc.data(),
+//   })) as Interview[];
+// }
+//
+//
+// // function to fetch interview details from firebase
+// export async function getInterviewById(id: string): Promise<Interview | null> {
+//   const interview = await db.collection('interviews').doc(id).get();
+//
+//   return interview.data() as Interview | null;
+// }
+//
+//
+// // Create a server action that will create feedback in firestore database using Gemini AI
+// export async function createFeedback(params: CreateFeedbackParams) {
+//
+//   const { interviewId, userId, transcript } = params;
+//
+//   try {
+// // content is what the person has answered
+//     const formattedTranscript = transcript
+//       .map(
+//         (sentence: { role: string; content: string }) =>
+//           `- ${sentence.role}: ${sentence.content}\n`,
+//       )
+//       .join('');
+//     const {
+//       object: {
+//         totalScore,
+//         categoryScores,
+//         strengths,
+//         areasForImprovement,
+//         finalAssessment,
+//       },
+//     } = await generateObject({
+//       model: google('gemini-2.0-flash-001', {
+//         structuredOutputs: false,
+//       }),
+//       // schema to specify how the text will look like
+//       schema: feedbackSchema,
+//       prompt: `
+//         You are an AI interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories. Be thorough and detailed in your analysis. Don't be lenient with the candidate. If there are mistakes or areas for improvement, point them out.
+//         Transcript:
+//         ${formattedTranscript}
+//
+//         Please score the candidate from 0 to 100 in the following areas. Do not add categories other than the ones provided:
+//         - **Communication Skills**: Clarity, articulation, structured responses.
+//         - **Technical Knowledge**: Understanding of key concepts for the role.
+//         - **Problem-Solving**: Ability to analyze problems and propose solutions.
+//         - **Cultural & Role Fit**: Alignment with company values and job role.
+//         - **Confidence & Clarity**: Confidence in responses, engagement, and clarity.
+//         `,
+//       // system to describe what is this AI
+//       system:
+//         'You are a professional interviewer analyzing a mock interview. Your task is to evaluate the candidate based on structured categories',
+//     });
+//
+//     const feedback = await db.collection('feedback').add({
+//       interviewId,
+//       userId,
+//       totalScore,
+//       categoryScores,
+//       strengths,
+//       areasForImprovement,
+//       finalAssessment,
+//       createdAt: new Date().toISOString(),
+//
+//     });
+//
+//     return {
+//       success: true,
+//       feedbackId: feedback.id,
+//     };
+//   } catch (err) {
+//     console.error('Error saving feedback', err);
+//     return { success: false };
+//   }
+//
+// }
+//
+// // function to fetch Feedback based on interview ID
+// export async function getFeedbackByInterviewId(params: GetFeedbackByInterviewIdParams): Promise<Feedback | null> {
+//   const { interviewId, userId } = params;
+//
+//   const feedback = await db.collection('feedback').where('interviewId', '==', interviewId).where('userId', '==', userId).limit(1).get();
+//
+//   if (feedback.empty) {
+//     return null;
+//   }
+//
+//   const feedbackDoc = feedback.docs[0];
+//   return {
+//     id: feedbackDoc.id, ...feedbackDoc.data(),
+//   } as Feedback;
+// }
